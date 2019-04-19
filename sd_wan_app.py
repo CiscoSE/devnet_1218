3b12aa2e25deb245ad2df76b6ee2b8a71968273d
Santiago Flores Kanter <sfloresk@cisco.com>
2019-04-19 16:04:08 -0600

    fix bugs

--- sd_wan_app.py
+++ sd_wan_app.py
@@ -28,22 +28,52 @@ def PrintMenu():
     Print menu in console
     :return:
     """
-    print("Please choose one option: ")
+    print("Please choose one option and press return: ")
     print("1- Get devices")
     print("2- Get control policies")
-    print("3- Get vpn memberships policies")
-    print("4- Get data policies")
-    print("5- Get VPN List Policies")
-    print("6- Get Site List Policies")
-    print("7- Get centralized policies")
-    print("8- Create FW Insertion Policy ")
-    print("9- Get interface statistics ")
-    print("10- Exit")
+    print("3- Get VPN List Policies")
+    print("4- Get Site List Policies")
+    print("5- Get centralized policies")
+    print("6- Create FW Insertion Policy ")
+    print("7- Get interface statistics ")
+    print("8- Exit")
+
+
+# ********* Step 2 - Get Devices
 
 
 
 
+# ********* Step 3 - Get Control Policies
 
+
+
+
+# ********* Step 4 - Get VPN List Policies
+
+
+
+
+# ********* Step 5 - Get Site List Policies
+
+
+
+
+# ********* Step 6 - Get Centralized Policies
+
+
+
+
+# ********* Step 7 - Create FW Insertion Policy
+
+
+
+
+# ********* Step 9 - - Get Interface Statistics
+
+
+
+# ********* DO NOT MODIFY CODE BELOW *********
 
 if __name__ == "__main__":
     """
@@ -78,24 +108,6 @@ if __name__ == "__main__":
                 print("\n")
 
             elif option == "3":
-                result = GetVPNMembershipPolicies()
-                policies = []
-                for policy in result["data"]:
-                    policies.append([policy["name"], policy["description"], policy["type"], policy["definitionId"]])
-                print("\nVPN Membership Policies:\n")
-                print(tabulate(policies, headers=['Policy Name', 'Description', 'Type', 'ID']))
-                print("\n")
-
-            elif option == "4":
-                result = GetDataPolicies()
-                policies = []
-                for policy in result["data"]:
-                    policies.append([policy["name"], policy["description"], policy["type"], policy["definitionId"]])
-                print("\nData Policies:\n")
-                print(tabulate(policies, headers=['Policy Name', 'Description', 'Type', 'ID']))
-                print("\n")
-
-            elif option == "5":
                 result = GetVPNListPolicies()
                 vpn_list = []
                 for vpn in result["data"]:
@@ -108,7 +120,7 @@ if __name__ == "__main__":
                 print(tabulate(vpn_list, headers=['VPN Name', 'Entries', 'Type', 'ID']))
                 print("\n")
 
-            elif option == "6":
+            elif option == "4":
                 result = GetSiteListPolicies()
                 site_list = []
                 for site in result["data"]:
@@ -120,7 +132,7 @@ if __name__ == "__main__":
                 print("\nSite List Policies:\n")
                 print(tabulate(site_list, headers=['Site List Name', 'Entries', 'Type', 'ID']))
                 print("\n")
-            elif option == "7":
+            elif option == "5":
                 result = GetCentralizedPolicies()
                 centralized_policies = []
                 for policy in result["data"]:
@@ -132,10 +144,10 @@ if __name__ == "__main__":
                 print("\nCentralized vSmart Policies:\n")
                 print(tabulate(centralized_policies, headers=['Policy Name', 'Description', 'Is Active?', 'ID']))
                 print("\n")
-            elif option == "8":
+            elif option == "6":
                 CreateFWCentrilizedPolicy()
 
-            elif option == "9":
+            elif option == "7":
                 result = GetInterfaceStatistics()
                 if_statistics = []
                 for if_statistic in result["data"]:
@@ -147,12 +159,12 @@ if __name__ == "__main__":
                         if_statistic["tx_errors"],
                         if_statistic["rx_pkts"],
                         if_statistic["rx_errors"]])
-                print("\nCentralized vSmart Policies:\n")
+                print("\nInterface statistics:\n")
                 print(tabulate(if_statistics,
                                headers=['Device', 'Interface', 'Status', 'TX Pkts', 'TX Errors', 'RX Pkts',
                                         'RX Errors']))
                 print("\n")
-            elif option == "10":
+            elif option == "8":
                 exit(1)
             else:
                 print("Invalid Option")
